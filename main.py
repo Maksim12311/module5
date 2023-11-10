7768854866bfa269b2cae1e1c54594bc0be04a17
Maksim12311 <max.brus.2016@gmail.com>
2023-11-10 19:23:08 +0300

    This is 1 task

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Here you can see the tasks for module5")
+class StringVar:
+	
+	def __init__(self, string):
+		self.string = string
+	
+	def set(self):
+		if self.string != []:
+			newarr = list(self.string)
+			del newarr[-1]
+			self.string = ''.join(newarr)
+
+	def get(self):
+		return self.string	
+
+s = StringVar(input())
+s.set()
+s.set()
+print(s.get())
