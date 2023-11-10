30daa35ef59cf9ba7c2f4747fb95fa3676d2d647
Maksim12311 <max.brus.2016@gmail.com>
2023-11-10 19:27:23 +0300

    This is third task

--- main.py
+++ main.py
@@ -1,22 +1,67 @@
-import math
+import random
 
-class Point:
+class Varrior:
 
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+	def __init__(self, name):
+		self.name = name
+		self.health = 100
+		self.tired = 100
+		self.armoar = 100
 
-	def __add__(self, other):
-		return Point(self.x + other.x, self.x + other.y)
+class Player1(Varrior):
 
-	def distance(self, other):
-		return math.sqrt((self.x - other.x)**2 + (self.y + other.y)**2)
+	def attack(self, opponent):
+		opponent.health -=random.randint(10,30) 
+		self.health -= random.randint(10,30)
+		self.tired -= 10
+		if self.health <=0:
+			print("Player1 is die")
+
+	def defend(self):
+		if self.armoar > 0:
+			self.armoar -=random.randint(0,10)
+			self.health -=random.randint(0,20)
+		else:
+			self.health -=random.randint(10,30)
+		if self.health <=0:
+			print("Player1 is die")
+		
+	def relax(self):
+		if self.tired < 90:
+			self.tired +=10
+		else:
+			self.tired = 100
 
 
+class Player2(Varrior):
 
-p1 = Point (1, 1)
-p2 = Point (2, 2)
-p3 = p1 +p2
-print(p3.x, p3.y)
-s = p1.distance(p2)
-print("Distance is", s)
+	def attack(self, opponent):
+		opponent.health -=random.randint(10,30)
+		self.health -= random.randint(10,30) 
+		self.tired -=10
+		if self.health <=0:
+			print("Player2 is die")
+
+	def defend(self):
+		if self.armoar > 0:
+			self.armoar -=random.randint(0,10)
+			self.health -=random.randint(0,20)
+		else:
+			self.health -=random.randint(10,30)
+		if self.health <=0:
+			print("Player2 is die")
+
+	def relax(self):
+		if self.tired < 90:
+			self.tired +=10
+		else:
+			self.tired = 100
+
+p1 = Player1('Player1')
+p2 = Player2('Player2')
+
+for i in range(3):
+	p1.attack(p2)
+	p2.attack(p1)
+
+print(p1.health, p2.health)
