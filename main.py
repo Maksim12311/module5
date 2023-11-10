a725adb2701b5b01f20b12ebe9e8e26a39ddfcb7
Maksim12311 <max.brus.2016@gmail.com>
2023-11-10 19:29:43 +0300

    This is fourth task

--- main.py
+++ main.py
@@ -1,67 +1,13 @@
-import random
+import json
+class Model:
 
-class Varrior:
+	def __init__(self,a):
+		self.a = a
 
-	def __init__(self, name):
-		self.name = name
-		self.health = 100
-		self.tired = 100
-		self.armoar = 100
+	def save(self,filename):
+		with open(filename,'w') as f:
+			attributes = [attr for attr in dir(self) if not attr.startswith("_")]
+            json.dump(attributes, f)
 
-class Player1(Varrior):
-
-	def attack(self, opponent):
-		opponent.health -=random.randint(10,30) 
-		self.health -= random.randint(10,30)
-		self.tired -= 10
-		if self.health <=0:
-			print("Player1 is die")
-
-	def defend(self):
-		if self.armoar > 0:
-			self.armoar -=random.randint(0,10)
-			self.health -=random.randint(0,20)
-		else:
-			self.health -=random.randint(10,30)
-		if self.health <=0:
-			print("Player1 is die")
-		
-	def relax(self):
-		if self.tired < 90:
-			self.tired +=10
-		else:
-			self.tired = 100
-
-
-class Player2(Varrior):
-
-	def attack(self, opponent):
-		opponent.health -=random.randint(10,30)
-		self.health -= random.randint(10,30) 
-		self.tired -=10
-		if self.health <=0:
-			print("Player2 is die")
-
-	def defend(self):
-		if self.armoar > 0:
-			self.armoar -=random.randint(0,10)
-			self.health -=random.randint(0,20)
-		else:
-			self.health -=random.randint(10,30)
-		if self.health <=0:
-			print("Player2 is die")
-
-	def relax(self):
-		if self.tired < 90:
-			self.tired +=10
-		else:
-			self.tired = 100
-
-p1 = Player1('Player1')
-p2 = Player2('Player2')
-
-for i in range(3):
-	p1.attack(p2)
-	p2.attack(p1)
-
-print(p1.health, p2.health)
+model_instance = Model(42)
+model_instance.save('output.json')
