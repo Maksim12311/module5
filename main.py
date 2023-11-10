48c9c9a623753094ec7a0cb1ed715571a644127a
Maksim12311 <max.brus.2016@gmail.com>
2023-11-10 19:25:24 +0300

    This is second task

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-class StringVar:
-	
-	def __init__(self, string):
-		self.string = string
-	
-	def set(self):
-		if self.string != []:
-			newarr = list(self.string)
-			del newarr[-1]
-			self.string = ''.join(newarr)
+import math
 
-	def get(self):
-		return self.string	
+class Point:
 
-s = StringVar(input())
-s.set()
-s.set()
-print(s.get())
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def __add__(self, other):
+		return Point(self.x + other.x, self.x + other.y)
+
+	def distance(self, other):
+		return math.sqrt((self.x - other.x)**2 + (self.y + other.y)**2)
+
+
+
+p1 = Point (1, 1)
+p2 = Point (2, 2)
+p3 = p1 +p2
+print(p3.x, p3.y)
+s = p1.distance(p2)
+print("Distance is", s)
